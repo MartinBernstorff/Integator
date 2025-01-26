18c129322476c0003871f147500674e167b4ffce
Martin Bernstorff <martinbernstorff@gmail.com>
2025-01-26 14:46:27 +0100

    update settings.json and settings.py

--- integator/settings.py
+++ integator/settings.py
@@ -107,7 +107,7 @@ class RootSettings(Settings):
     # Could remove a layer of indirection.
     integator: IntegatorSettings = IntegatorSettings()
 
-    # XXX: Log some info when init'ing here
+    # feat: Log some info when init'ing here
 
     def write_toml(self, path: pathlib.Path):
         values = json.loads(self.model_dump_json())
